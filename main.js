dfba80b1fda208a545bcbf2395d93bd271735a04
Ilan <ilanluciano1@gmail.com>
2022-12-12 17:53:56 -0500

    start unifying styling theme

--- main.js
+++ main.js
@@ -45,7 +45,7 @@ function load() {
                     <div class="checkbox ${save.items[item].checked}" onClick="checkItem(event)"></div>
                     <span class="${save.items[item].checked}">${save.items[item].name}</span>
 
-                    <svg xmlns="http://www.w3.org/2000/svg" onclick="deleteItem(event)"  width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                    <svg id="delete" xmlns="http://www.w3.org/2000/svg" onclick="deleteItem(event)"  width="20" height="20" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
                         <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5ZM11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0H11Zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5h9.916Zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5Z"/>
                     </svg>
 
@@ -56,7 +56,7 @@ function load() {
                     <span class="focused" id="focus-${save.items[item].name}">
 
                         <p onclick="edit();">${save.items[item].description || 'description'}</p>
-                        <svg style="margin-right: -160px; margin-top: -35px;" onclick="edit();" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
+                        <svg style="margin-right: -160px; margin-top: -35px;" onclick="edit();" xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-pencil-square" viewBox="0 0 16 16">
                             <path d="M15.502 1.94a.5.5 0 0 1 0 .706L14.459 3.69l-2-2L13.502.646a.5.5 0 0 1 .707 0l1.293 1.293zm-1.75 2.456-2-2L4.939 9.21a.5.5 0 0 0-.121.196l-.805 2.414a.25.25 0 0 0 .316.316l2.414-.805a.5.5 0 0 0 .196-.12l6.813-6.814z"/>
                             <path fill-rule="evenodd" d="M1 13.5A1.5 1.5 0 0 0 2.5 15h11a1.5 1.5 0 0 0 1.5-1.5v-6a.5.5 0 0 0-1 0v6a.5.5 0 0 1-.5.5h-11a.5.5 0 0 1-.5-.5v-11a.5.5 0 0 1 .5-.5H9a.5.5 0 0 0 0-1H2.5A1.5 1.5 0 0 0 1 2.5v11z"/>
                         </svg>
@@ -106,7 +106,7 @@ function open(type) {
     document.getElementById('popup').style.display = 'block';
     document.getElementById('item-name').select();
 
-    if (localStorage.getItem('selected') == 'upcoming') {
+    if (localStorage.getItem('selected') == 'list_upcoming') {
         document.getElementById('date-input').style.display = 'block';
     } else {
         document.getElementById('date-input').style.display = 'none';
@@ -273,22 +273,4 @@ function newTopic() {
     topics.push(name);
 
     localStorage.setItem('topics', JSON.stringify(topics));
-}
-
-// display keybind info when keyboard icon clicked
-function showControls() {
-    openInfoModal(`
-        'n' -> create new item <br>
-        'escape' -> exit popup <br>
-        'enter' -> submit <br>
-        'left arrow' -> switch to sidebar navigation <br>
-        'right arrow' -> switch to list item navigation <br>
-        <b>sidebar navigation</b> <br>
-        'up arrow' -> navigate to list above on sidebar <br>
-        'down arrow' -> navigate to list below on sidebar <br>
-        <b>list item navigation</b> <br>
-        'up arrow' -> navigate to previous item <br>
-        'down arrow' -> navigate to next item <br>
-        'e' -> edit selected items description <br>
-    `);
 }
